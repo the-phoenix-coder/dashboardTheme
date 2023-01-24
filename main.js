fec7a23c3d015dbb2de72859d9c9545bc0ad056f
the-phoenix-coder <ahmedhesham1223339@gmail.com>
2023-01-24 22:10:41 +0200

    Social Info Were Added

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ btnToggle.forEach(btn =>
 {
     btn.addEventListener('click', () => btn.classList.toggle('active'))
 })
+
+const inputs = document.querySelectorAll('.social-info .social-box input')
+
+inputs.forEach(input =>
+{
+    input.addEventListener('focus', () =>
+    {
+        inputs.forEach(input => input.parentElement.children[0].style.fill = '#999')
+        input.parentElement.children[0].style.fill = '#000'
+    })
+        input.addEventListener('focusout', () => inputs.forEach(input => input.parentElement.children[0].style.fill = '#999'))
+})
